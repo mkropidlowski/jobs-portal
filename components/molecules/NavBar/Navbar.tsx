425373298b7b0fd97eb8f97397f3d448dc5b67b2
mkropidlowski <mkropidlowsky@gmail.com>
2023-03-05 00:04:49 +0100

    JOB-2: Create navbar component

---
+++ components/molecules/NavBar/Navbar.tsx
@@ -0,0 +1,33 @@
+import styles from './navbar.module.scss';
+import { menuLinks } from './helpers';
+import Link from 'next/link';
+import Button from '../../atoms/Button/Button';
+
+export interface INavbarProps {
+	links?: INavbarProps[];
+	text?: string;
+}
+
+const Navbar: React.FC<INavbarProps> = ({ links = menuLinks }) => {
+	return (
+		<nav className={styles.container}>
+			<ul>
+				{Object.values(links).map(({ id, text }) => {
+					const linkHref = `/#${id}`;
+					return (
+						<li key={text}>
+							<Link href={linkHref}>
+								<Button type="button" color="secondary">
+									{text}
+								</Button>
+							</Link>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
+
---
+++ components/templates/base/Base.mocks.ts
@@ -0,0 +1,9 @@
+import { IBase } from './Base';
+
+const base: IBase = {
+	sampleTextProp: 'Hello world!',
+};
+
+export const mockBaseTemplateProps = {
+	base,
+};
