1804befa0d9471312908ad0729f9a2081406a89b
mkropidlowski <mkropidlowsky@gmail.com>
2023-03-12 13:44:22 +0100

    JOB-12: Fixed build error

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,11 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { fakeData } from 'components/organisms/Offers/data/fakeData';
-import { ICard } from 'components/organisms/Offers/types';
 type Data = {
-	offersList: ICard[];
+	name?: string;
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	res.status(200).json({ offersList: fakeData });
+	res.status(200).json({ name: 'john' });
 }
