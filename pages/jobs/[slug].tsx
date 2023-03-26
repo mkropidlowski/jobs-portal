41a57fc855bf6b83a84f7090284c23ab2372f728
mkropidlowski <mkropidlowsky@gmail.com>
2023-03-26 12:19:24 +0200

    JOB-18: Fixed err

--- pages/jobs/[slug].tsx
+++ pages/jobs/[slug].tsx
@@ -2,10 +2,12 @@ import JobDetails from 'components/molecules/JobDetails/JobDetails';
 import { ICard } from 'components/organisms/Offers/types';
 import { useRouter } from 'next/router';
 
-export const JobDetailsPage = () => {
+const JobDetailsPage = () => {
 	const router = useRouter();
 	const { slug } = router.query as ICard;
 
 	return <JobDetails jobId={slug} />;
 };
+
+export default JobDetailsPage;
 
